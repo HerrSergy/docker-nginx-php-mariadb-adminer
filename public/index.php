2b4563ab7ea7e0ff6f15372a65c53d178b3ddce2
Sergio <61321287+HerrSergy@users.noreply.github.com>
2023-07-26 19:58:42 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-phpinfo();
-
-/*
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $mysqli = new mysqli('172.25.0.2', 'admin', 'admin123', 'twitter');
@@ -10,4 +7,4 @@ $mysqli = new mysqli('172.25.0.2', 'admin', 'admin123', 'twitter');
 // Set the desired charset after establishing a connection
 $mysqli->set_charset('utf8mb4');
 
-printf("Success... %s\n", $mysqli->host_info);*/
+printf("Success... %s\n", $mysqli->host_info);
